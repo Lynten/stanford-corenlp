c1158d35eae5073b82fbe106c9d66a580846887f
Sandeep Subramanian <sandeeps94@cse.iitb.ac.in>
2019-01-21 16:50:31 +0530

    Attempt cleanup when reference count goes to zero
    Attempt to cleanup the StanfordCoreNLP object when it is no
    longer referenced.

--- stanfordcorenlp/corenlp.py
+++ stanfordcorenlp/corenlp.py
@@ -127,6 +127,9 @@ class StanfordCoreNLP:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
 
+    def __del__(self):
+        self.close()
+
     def close(self):
         logging.info('Cleanup...')
         if hasattr(self, 'p'):
